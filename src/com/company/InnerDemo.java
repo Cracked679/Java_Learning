fd36d7ae150a67dff438c71c91240b2b15ed295c
Cracked679 <shyambodke679@gmail.com>
2018-04-19 23:49:30 +0530

    Inner Class Demo

--- src/com/company/InnerDemo.java
+++ src/com/company/InnerDemo.java
@@ -1,4 +1,27 @@
 package com.company;
 
+class outerClass{
+    int a;
+
+    public void show(){
+
+    }
+
+    class innerClass{
+        public void display(){
+            System.out.println("In display");
+        }
+    }
+}
+
 public class InnerDemo {
+    public static void main(String[] args) {
+
+        outerClass obj1=new outerClass();
+        obj1.show();
+
+        outerClass.innerClass obj2 = obj1.new innerClass();
+        obj2.display();
+
+    }
 }
--- src/com/company/StaticDemo.java
+++ src/com/company/StaticDemo.java
@@ -2,7 +2,6 @@ package com.company;
 
 class staticDemo{
 
-    
 }
 public class StaticDemo {
 }
