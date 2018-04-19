eda808af5b539753b0c52cc1ad6482c34c210e47
Cracked679 <shyambodke679@gmail.com>
2018-04-19 23:15:30 +0530

    Class object Demo

--- src/com/company/ClassObjectDemo.java
+++ src/com/company/ClassObjectDemo.java
@@ -1,8 +1,23 @@
 package com.company;
 
+class student{
+    String snm,sdiv;
+    int rno;
+
+    public void display(){
+        System.out.println(snm+" : "+sdiv+" : "+rno);
+    }
+}
 public class ClassObjectDemo {
 
     public static void main(String[] args) {
 	// write your code here
+        student obj1 = new student();
+
+        obj1.snm="Shyam";
+        obj1.sdiv="A";
+        obj1.rno=20;
+
+        obj1.display();
     }
 }
---
+++ src/com/company/StaticDemo.java
@@ -0,0 +1,8 @@
+package com.company;
+
+class staticDemo{
+
+    
+}
+public class StaticDemo {
+}
